68b0de040d9c8077d7b6184822409fe2b4ef8365
wonil <dnjsdlfdld@gmail.com>
2025-11-06 00:53:44 +0900

    1. bottom_nav_icon add

---
+++ lib/app/core/constants/app_images.dart
@@ -0,0 +1,15 @@
+abstract class AppImages {
+  AppImages._();
+
+  static const _baseSvgPath = "assets/images/svg";
+  static const _basePngPath = "assets/images/png";
+
+  static const inActiveReceive = "$_baseSvgPath/bottom_nav/in_active_receive.svg";
+  static const activeReceive = "$_baseSvgPath/bottom_nav/active_receive.svg";
+
+  static const inActiveSend = "$_baseSvgPath/bottom_nav/in_active_send.svg";
+  static const activeSend = "$_baseSvgPath/bottom_nav/active_send.svg";
+
+  static const inActiveMyPage = "$_baseSvgPath/bottom_nav/in_active_my_page.svg";
+  static const activeMyPage = "$_baseSvgPath/bottom_nav/active_my_page.svg";
+}
--- lib/app/presentation/main/send/send_view.dart
+++ lib/app/presentation/main/send/send_view.dart
@@ -1,6 +1,8 @@
 import 'package:app_gift_log/app/core/base/base_view.dart';
+import 'package:app_gift_log/app/core/constants/app_images.dart';
 import 'package:app_gift_log/app/presentation/main/send/send_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SendView extends BaseView<SendViewModel> {
   const SendView({super.key});
@@ -12,6 +14,15 @@ class SendView extends BaseView<SendViewModel> {
 
   @override
   Widget body(BuildContext context) {
-    return Center(child: Text("SendView"));
+    return Center(child: Row(
+      children: [
+        SvgPicture.asset(AppImages.inActiveReceive),
+        SvgPicture.asset(AppImages.activeReceive),
+        SvgPicture.asset(AppImages.inActiveSend),
+        SvgPicture.asset(AppImages.activeSend),
+        SvgPicture.asset(AppImages.inActiveMyPage),
+        SvgPicture.asset(AppImages.activeMyPage),
+      ],
+    ));
   }
 }
